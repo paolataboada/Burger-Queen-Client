e64164d13493cec4b2263e7ab4e6461e9dd736c0
BethzyOrtega <bethzykathia@gmail.com>
2021-12-09 18:45:20 -0500

    add orders cards

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export const Form = () => {
+    const [credenciales, setCredenciales] = useState({
+        name : '',
+        password:'',
+    });
+
+    const handleCHange = (e) => {
+        e.preventDefault();
+        setCredenciales({
+            ...credenciales,
+            [e.target.name] : e.target.value,
+        })
+        console.log(credenciales);
+    };
+
+    const btnLogin = (e) => {
+        e.preventDefault();
+        const URL = 'https://bq-lim015.herokuapp.com/auth';
+        console.log(credenciales);
+       axios.post(URL, credenciales).then((res) => {
+        if (res.request.status === 200){
+            console.log(res.data.token);
+           localStorage.setItem('token', res.data.token);
+           // console.log(JSON.parse(localStorage.setItem('token', res.data.token)));
+        //    props.history.push('/menu');
+        }
+    })
+    .catch((res) => console.log(res));
+    }
+    
+
+
     return (
         <>
             <form className="d-grid gap-2 col-9 mx-auto form-login">
                 <div className="form-group row">
                     <label htmlFor="inputEmail3" className="col-sm-3 col-form-label">Email</label>
                     <div className="col-sm-9">
-                        <input type="email" className="form-control" id="inputEmail3" placeholder="Email"/>
+                        <input onChange={handleCHange} type="email" name='name' className="form-control" id="inputEmail3" placeholder="Email"/>
                     </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="inputPassword3" className="col-sm-3 col-form-label">Password</label>
                     <div className="col-sm-9">
-                        <input type="password" className="form-control" id="inputPassword3" placeholder="Password"/>
+                        <input type="password" onChange={handleCHange} name='password' className="form-control" id="inputPassword3" placeholder="Password"/>
                     </div>
                 </div>
                 <div className="form-group row">
                     <div className="d-grid col-5 mx-auto">
-                        <button type="submit" className="btn btn-warning btn-login">Log In</button>
+                        <button onClick={btnLogin} type="submit" className="btn btn-warning btn-login">Log In</button>
                     </div>
                 </div>
             </form>
--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
-import { Navbar } from './Navbar';
+import React from "react";
+import { Navbar } from "./Navbar";
+import '../styles/view-productos.css';
 
 export const Orders = () => {
-    return (
-        <>
-            <Navbar />
-            <div>
-                ORDENES
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <section className="container-orders">
+      <div>
+          <h1 className="pending">Pending</h1>
+        <div className="card text-center card-list-orders">
+         <div className="card-header">
+          NAme
+         </div>
+         <div className="card-body">
+          <p className="card-text">lo</p>
+          <button className="btn btn-primary">Mover</button>
+          <button className="btn btn-primary">Cancelar</button>
+         </div>
+        </div>
+      </div>        
+
+      <div>
+          <h1 className="delivering">Delivering</h1>
+        <div className="card text-center card-list-orders">
+         <div className="card-header">
+          NAme
+         </div>
+         <div className="card-body">
+          <p className="card-text">lo</p>
+          <button className="btn btn-primary">Mover</button>
+          <button className="btn btn-primary">Cancelar</button>
+         </div>
+        </div>
+      </div>
+
+      <div>
+          <h1 className="delivered">Delivered</h1>
+        <div className="card text-center card-list-orders">
+         <div className="card-header">
+          NAme
+         </div>
+         <div className="card-body">
+          <p className="card-text">lo</p>
+          <button className="btn btn-primary">Mover</button>
+          <button className="btn btn-primary">Cancelar</button>
+         </div>
+        </div>
+      </div>
+ 
+      </section>
+    </>
+  );
+};
