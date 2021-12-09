fcf243e4e94f202849aa7d67ad9b12556188ce80
paolataboada <taboadapaola3@gmail.com>
2021-12-08 22:45:31 -0500

    add styles to login view

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,23 +3,23 @@ import React from 'react';
 export const Form = () => {
     return (
         <>
-            <form>
+            <form className="d-grid gap-2 col-9 mx-auto" style={{border: "3px solid #FFAB25", borderRadius: "25px", padding: "50px 20px", backgroundColor: "white"}}>
                 <div className="form-group row">
-                <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
-                <div className="col-sm-10">
-                    <input type="email" className="form-control" id="inputEmail3" placeholder="Email" />
-                </div>
-                </div>
-                <div className="form-group row">
-                <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
-                <div className="col-sm-10">
-                    <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
-                </div>
+                    <label htmlFor="inputEmail3" className="col-sm-3 col-form-label">Email</label>
+                    <div className="col-sm-9">
+                        <input type="email" className="form-control" id="inputEmail3" placeholder="Email" style={{border: "1px solid #FFAB25"}}/>
+                    </div>
                 </div>
                 <div className="form-group row">
-                <div className="col-sm-10">
-                    <button type="submit" className="btn btn-success">Sign in</button>
+                    <label htmlFor="inputPassword3" className="col-sm-3 col-form-label">Password</label>
+                    <div className="col-sm-9">
+                        <input type="password" className="form-control" id="inputPassword3" placeholder="Password" style={{border: "1px solid #FFAB25"}}/>
+                    </div>
                 </div>
+                <div className="form-group row">
+                    <div className="d-grid col-5 mx-auto">
+                        <button type="submit" className="btn btn-success" style={{border: "1px solid #FFAB25", backgroundColor: "#FFAB25"}}>Log In</button>
+                    </div>
                 </div>
             </form>
         </>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Form } from './Form';
+import burger from '../assets/burger-cover.png'
 
 export const Login = () => {
     return (
         <>
-            <Form />
+            <div style={{display: "flex", alignItems: "center", height: "100vh"}}>
+                <div style={{width: "40%", paddingLeft: "20px"}}>
+                    <img width="100%" src={burger} alt="Hamburguesa de portada" />
+                </div>
+                <div style={{width: "60%"}} className="container-fluid">
+                    <div style={{display: "flex", flexFlow: "column nowrap", alignItems: "center"}}>
+                        <h3 style={{color: "#FFAB25"}}>Welcome back!</h3>
+                        <p style={{color: "white"}}>Login to continue Burger City</p>
+                    </div>
+                    <Form />
+                </div>
+            </div>
         </>
     )
 }
